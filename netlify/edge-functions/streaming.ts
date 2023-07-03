7f786fd2722d3e110f4aaf2827a4138e4ef9c527
Manu MA <manu.mtza@gmail.com>
2023-07-03 10:27:20 +0200

    👑

--- netlify/edge-functions/streaming.ts
+++ netlify/edge-functions/streaming.ts
@@ -4,15 +4,23 @@ export default async () => {
   const { readable, writable } = new TransformStream<Uint8Array>();
   const encoder = new TextEncoder();
   const writer = writable.getWriter();
-  let index = 0;
-  setInterval(() => {
-    writer.write(encoder.encode(`data: Hello ${index++}\n\n`))
-    index++;
-  }, 1000);
+  async function run() {
+    let index = 0;
+    while(true) {
+      await delay(1000);
+      await writer.write(encoder.encode(`data2: Hello ${index++}\n\n`))
+      index++;
 
+    }
+  }
+  run();
   return new Response(readable, {
     headers: {
       "Content-Type": "text/event-stream",
     },
   });
 };
+
+function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
